004325318eb81e7a826d57e531f964f2962a4190
elsie-dev <elcymao@gmail.com>
2021-01-14 01:53:50 -0800

    changing the way to check user's os

---
+++ sca-cloud-application/classmodule.py
@@ -0,0 +1,6 @@
+class operating_system():
+    def __init__(self, name):
+        self.name = windows
+
+    def windows(self):
+        print('os is {}'.format(self.name))
--- sca-cloud-application/cli.py
+++ sca-cloud-application/cli.py
@@ -1,34 +1,23 @@
 import sys
 from sys import platform
-
-
-def main():
-    print('in main')
-    # args = sys.argv[1:]
-    # print('count of args :: {}'.format(len(args)))
-    # for arg in args:
-    #     print('passed argument :: {}'.format(arg))
-
-    # my_function('hello world')
-
-    # my_object = MyClass('Thomas')
-    # my_object.say_name()
-
-
-    
+ 
 if sys.platform =="linux" or platform== "linux2":
-   print ("linux")
+   print ("The os is Linux")
    
 elif sys.platform =="darwin":
-   print ("windows")
+   print ("The os is Windows")
    
-
 else:
  sys.platform =="win32"
- print ("mac")
+ print ("The os is Mac")
   
+# args = sys.argv[1:]
+# def install_software(self):
+#  for arg in args:
+#   print('softwares installed are:{}'.format(len(args)))
+  
+# software = ["node.js"]
 
+# if __name__== "__main__":
+#    software()
 
-
-if __name__== "__main__":
-    main()
